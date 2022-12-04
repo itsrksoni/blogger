e8a4608949324aeb721b8ef1e1233a358dfdf90a
Rohit Kumar <itsrksoni@gmail.com>
2022-12-04 15:57:14 +0000

    web-based create comment feature added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-    resources :articles, except: [:destroy]
+    resources :articles, except: [:destroy] do
+        resources :comments, only: [:create]
+    end
     delete 'articles/:id/delete' => 'articles#destroy', as: 'articles_delete'
     get '/articles/:id/delete' => 'articles#destroy'
 
