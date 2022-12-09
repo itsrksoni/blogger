338ca7c47d08fbaa012812c7a71c634c78beae1c
Rohit Kumar <itsrksoni@gmail.com>
2022-12-09 08:42:05 +0000

    reply to comment feature added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_04_191621) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_09_050031) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -24,7 +24,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_04_191621) do
     t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "parent_id"
     t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["parent_id"], name: "index_comments_on_parent_id"
   end
 
   create_table "taggings", force: :cascade do |t|
@@ -43,6 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_04_191621) do
   end
 
   add_foreign_key "comments", "articles"
+  add_foreign_key "comments", "comments", column: "parent_id"
   add_foreign_key "taggings", "articles"
   add_foreign_key "taggings", "tags"
 end
